10efc423813634a98f64536e81be605b785e1ab8
ldebey <louis.debey@hotmail.fr>
2023-12-27 14:19:57 +0100

    Bait server

--- index.js
+++ index.js
@@ -121,3 +121,16 @@ for (const file of eventFiles) {
 
 
 client.login(token);
+
+// for bait server
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
