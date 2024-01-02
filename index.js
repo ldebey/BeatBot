8b187e75b6fe7c49ed87456bc5c72a29e4a676e7
ldebey <louis.debey@hotmail.fr>
2024-01-02 19:54:45 +0100

    Remove useless things

--- index.js
+++ index.js
@@ -121,16 +121,3 @@ for (const file of eventFiles) {
 
 
 client.login(token);
-
-// for bait server
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
